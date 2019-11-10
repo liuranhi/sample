fba7a7ec4dac71ec9d870216c3d1ff96a0ca6c0f
nishizumi <daiman003@yahoo.co.jp>
2019-11-10 18:25:45 +0900

    add sobel

--- python/opencv/filter2d/sobel/sobel1.py
+++ python/opencv/filter2d/sobel/sobel1.py
@@ -23,19 +23,19 @@ def filter2d(src, kernel):
 
 
 # 入力画像を読み込み
-img = cv2.imread("C:/github/sample/python/opencv/filter2d/diff/input.png")
+img = cv2.imread("C:/github/sample/python/opencv/filter2d/sobel/input.png")
 
 # グレースケール変換
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
 # カーネル（水平、垂直方向の輪郭検出用）
-kernel_x = np.array([[0, -1, 0],
-                     [0, 0, 0],
-                     [0, 1, 0]])
+kernel_x = np.array([[-1, 0, 1],
+                   [-2, 0, 2],
+                   [-1, 0, 1]])
 
-kernel_y = np.array([[0, 0, 0],
-                     [-1, 0, 1],
-                     [0, 0, 0]])
+kernel_y = np.array([[-1, -2, -1],
+                     [0, 0, 0],
+                     [1, 2, 1]])
 
 # 方法1
 gray_x = filter2d(gray, kernel_x)
@@ -43,4 +43,4 @@ gray_y = filter2d(gray, kernel_y)
 dst = np.sqrt(gray_x ** 2 + gray_y ** 2)
 
 # 結果を出力
-cv2.imwrite("C:/github/sample/python/opencv/filter2d/diff/output.png", dst)
+cv2.imwrite("C:/github/sample/python/opencv/filter2d/sobel/output.png", dst)
