4fa3e28479b57639dbd1bb4b17da104fabaf144b
nishizumi <daiman003@yahoo.co.jp>
2019-11-17 13:03:59 +0900

    no message

--- flutter/basic/lib/main.dart
+++ flutter/basic/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:basic/ui/home_page.dart';
-import 'package:basic/ui/listview_card_page.dart';
+//import 'package:basic/ui/home_page.dart';
+//import 'package:basic/ui/listview_card_page.dart';
+import 'package:basic/ui/listview_listtitle_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Test App',
       theme: new ThemeData.dark(),
-      home: HomePage(title: 'Home Page'),
+      // home: HomePage(title: 'Home Page'),
       // home:ListviewCardPage(title: 'Listview Card Page'),
+      home:ListviewListtitlePage(title: 'Listview Listtitle Page'),
     );
   }
 }
