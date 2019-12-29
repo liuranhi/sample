71bee20e7653e68b17ff0f4f14b1a8f636e9a573
nishizumi <daiman003@yahoo.co.jp>
2019-12-29 18:00:47 +0900

    no message

--- flutter/navigator/lib/screens/home_page.dart
+++ flutter/navigator/lib/screens/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  int _currentIndex = 0; // currentIndexにデフォルト値を与えないとコンパイルエラーにな
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -25,10 +25,35 @@ class _HomePageState extends State<HomePage> {
               // ①ボタンでページ遷移
               // 遷移するにはNavigator.of(context).pushNamedに対して遷移先の名称を渡すことで、対象のウィジェットを呼び出し
               // 呼び出されたウィジェットはHomePageの上にSettingPageになりナビゲーションヘッダーに戻るボタンが表示される
-              RaisedButton(onPressed: () => Navigator.of(context).pushNamed("/setting"), child: new Text('Subページへ'),)
+              RaisedButton(onPressed: () => Navigator.of(context).pushNamed("/setting"), child: new Text('設定ページ'),)
             ],
           ),
         ),
+      ),
+      
+      // ② 下部ナビゲーションバーでページ遷移
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.home),
+              title: Text("ホーム")
+          ),
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.settings),
+              title: Text("設定")
+          ),
+        ],
+        // ナビゲーションバーのいずれかのボタンがタップされたら
+        onTap: (int index) {
+          print(index); // デバッグ用に出力（タップされたボタンによって数値がかわる）
+          if(index == 0){
+            Navigator.of(context).pushNamed("/home");
+          }
+          else if(index == 1){
+            Navigator.of(context).pushNamed("/setting");
+          }
+        },
       ),
     );
   }
