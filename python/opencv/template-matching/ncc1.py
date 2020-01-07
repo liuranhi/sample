f316ed2b9f5c24a31463732611f0d48ea9dfeeda
nishizumi <daiman003@yahoo.co.jp>
2020-01-07 22:38:50 +0900

    no message

--- python/opencv/template-matching/ncc1.py
+++ python/opencv/template-matching/ncc1.py
@@ -2,39 +2,22 @@
 import cv2
 import numpy as np
 
-# 最近傍補間法でリサイズ
-def resize_nearest(src, h, w):
-    # 出力画像用の配列生成（要素は全て空）
-    dst = np.empty((h,w))
+# 入力画像とテンプレート画像をで取得
+img = cv2.imread("C:/github/sample/python/opencv/template-matching/input.png")
+temp = cv2.imread("C:/github/sample/python/opencv/template-matching/temp.png")
 
-    # 元画像のサイズを取得
-    hi, wi = src.shape[0], src.shape[1]
-
-    # 拡大率を計算
-    ax = w / float(wi)
-    ay = h / float(hi)
-
-    # 最近傍補間
-    for y in range(0, h):
-        for x in range(0, w):
-            xi, yi = int(round(x/ax)), int(round(y/ay))
-            # 存在しない座標の処理
-            if xi > wi -1: xi = wi -1
-            if yi > hi -1: yi = hi -1
-
-            dst[y][x] = src[yi][xi]
-
-    return dst
-
-
-# 入力画像の読み込み
-img = cv2.imread("C:/github/sample/python/opencv/resize/input.png")
-    
 # グレースケール変換
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+temp = cv2.cvtColor(temp, cv2.COLOR_RGB2GRAY)
 
-# 方法1(NumPy）
-dst = resize_nearest(gray, gray.shape[1]*2, gray.shape[0]*2)
+# テンプレート画像の高さ・幅
+h, w = temp.shape
 
-# 結果を出力
-cv2.imwrite("C:/github/sample/python/opencv/resize/nearest1.png", dst)
+# テンプレートマッチング（OpenCVで実装）
+match = cv2.matchTemplate(gray, temp, cv2.TM_CCOEFF_NORMED)
+min_value, max_value, min_pt, max_pt = cv2.minMaxLoc(match)
+pt = max_pt
+
+# テンプレートマッチングの結果を出力
+cv2.rectangle(img, (pt[0], pt[1]), (pt[0] + w, pt[1] + h), (0, 0, 200), 3)
+cv2.imwrite("C:/github/sample/python/opencv/template-matching/ncc1.png", img)
