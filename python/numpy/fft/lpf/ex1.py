26442e0b7a08e3160f31d15aec7876ba48077d72
nishizumi <daiman003@yahoo.co.jp>
2020-01-26 16:18:05 +0900

    no message

--- python/numpy/fft/lpf/ex1.py
+++ python/numpy/fft/lpf/ex1.py
@@ -6,12 +6,15 @@ import matplotlib.pyplot as plt
 N = 256            # サンプル数
 dt = 0.01          # サンプリング間隔
 fq1, fq2 = 5, 40    # 周波数
-fc = 20            # カットオフ周波数
+fc = 20  # カットオフ周波数
+A1, A2 = 20, 5
 t = np.arange(0, N*dt, dt)  # 時間軸
 freq = np.linspace(0, 1.0/dt, N)  # 周波数軸
 
 # 時間信号（周波数5の正弦波 + 周波数40の正弦波）の生成
-f = np.sin(2*np.pi*fq1*t) + 0.2 * np.sin(2*np.pi*fq2*t)
+f1 = A1 * np.sin(2 * np.pi * fq1 * t)
+f2 = A2 * np.sin(2*np.pi*fq2*t)
+f = f1 + f2
 
 # 高速フーリエ変換（周波数信号に変換）
 F = np.fft.fft(f)
@@ -33,43 +36,43 @@ f2 = np.fft.ifft(F2)
 f2 = np.real(f2*N)
 
 # グラフ表示
-plt.figure()
+fig = plt.figure(figsize=(10.0, 8.0))
 plt.rcParams['font.family'] = 'Times New Roman'
-plt.rcParams['font.size'] = 17
+plt.rcParams['font.size'] = 12
 
 # 時間信号（元）
 plt.subplot(221)
 plt.plot(t, f, label='f(n)')
-plt.xlabel("Time", fontsize=20)
-plt.ylabel("Signal", fontsize=20)
+plt.xlabel("Time", fontsize=12)
+plt.ylabel("Signal", fontsize=12)
 plt.grid()
-leg = plt.legend(loc=1, fontsize=25)
+leg = plt.legend(loc=1, fontsize=15)
 leg.get_frame().set_alpha(1)
 
 # 周波数信号(元)
 plt.subplot(222)
 plt.plot(freq, np.abs(F), label='|F(k)|')
-plt.xlabel('Frequency', fontsize=20)
-plt.ylabel('Amplitude', fontsize=20)
+plt.xlabel('Frequency', fontsize=12)
+plt.ylabel('Amplitude', fontsize=12)
 plt.grid()
-leg = plt.legend(loc=1, fontsize=25)
+leg = plt.legend(loc=1, fontsize=15)
 leg.get_frame().set_alpha(1)
 
 # 時間信号(処理後)
 plt.subplot(223)
 plt.plot(t, f2, label='f2(n)')
-plt.xlabel("Time", fontsize=20)
-plt.ylabel("Signal", fontsize=20)
+plt.xlabel("Time", fontsize=12)
+plt.ylabel("Signal", fontsize=12)
 plt.grid()
-leg = plt.legend(loc=1, fontsize=25)
+leg = plt.legend(loc=1, fontsize=15)
 leg.get_frame().set_alpha(1)
 
 # 周波数信号(処理後)
 plt.subplot(224)
 plt.plot(freq, np.abs(F2), label='|F2(k)|')
-plt.xlabel('Frequency', fontsize=20)
-plt.ylabel('Amplitude', fontsize=20)
+plt.xlabel('Frequency', fontsize=12)
+plt.ylabel('Amplitude', fontsize=12)
 plt.grid()
-leg = plt.legend(loc=1, fontsize=25)
+leg = plt.legend(loc=1, fontsize=15)
 leg.get_frame().set_alpha(1)
-plt.show()
+plt.savefig('C:/github/sample/python/numpy/fft/lpf/ex1.png')
