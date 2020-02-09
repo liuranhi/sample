f3f9ea6b948ed876d3abcf239f030ba573658dd2
nishizumi <daiman003@yahoo.co.jp>
2020-02-09 09:23:56 +0900

    no message

--- python/file/search_words.py
+++ python/file/search_words.py
@@ -7,10 +7,20 @@ keyword = "test"
 filepath = "C:/github/sample/python/file/sample.txt"
 
 # ファイルの中身を1行ずつ読み込んでリストに格納
-with open(, mode='r', newline='', encoding='utf-8') as f_in:
+with open(filepath, mode='r', newline='', encoding='utf-8') as f_in:
     lines = [line for line in f_in]
 
 # リストから1行文ずつデータを取り出し、検索ワードが含まれているかチェック
 for i in lines:
     if keyword in i:
-        print(lines.index(i)) 
+        print("行番号：", lines.index(i+1))  # 行番号： 3
+
+"""
+sample.txt
+
+Exdfhsaoipva
+vhgsklgha:
+nas:glkwnhas:gva]
+ashjapghatestdfa
+ajslkgahjga
+"""
