b2d261205956e386ef839361284ae2f93a12a095
nishizumi <daiman003@yahoo.co.jp>
2020-02-23 09:49:14 +0900

    no message

--- php/laravel/bbs/database/factories/CommentFactory.php
+++ php/laravel/bbs/database/factories/CommentFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Comment::class, function (Faker $faker) {
     return [
-        'body' => "コメントです。テキストテキストテキストテキストテキストテキスト。\nテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。",
+        'body' => "千早ぶる神代もきかず龍田川からくれなゐに水くくるとは",
     ];
 });
